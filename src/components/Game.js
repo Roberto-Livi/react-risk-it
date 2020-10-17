0c7e9c4f0b498b28c1db6375efc5f671fd73dd35
Roberto-Livi <robertomlivi@outlook.com>
2020-10-16 21:19:36 -0400

    able to submit a score to bankAccount

--- src/components/Game.js
+++ src/components/Game.js
@@ -5,8 +5,9 @@ class Game extends React.Component {
     state = {
         currentScore: 0,
         randomScore: 0,
-        totalScore: 0,
-        chosenAmount: 0
+        bankAccount: 0,
+        chosenAmount: 0,
+        counter: 0
     }
 
     componentDidMount() {
@@ -19,23 +20,65 @@ class Game extends React.Component {
 
     onSubmit = (event) => {
         event.preventDefault()
-        let { currentScore, chosenAmount, randomScore } = this.state
-        let summedAmount;
-        if(parseInt(chosenAmount) < randomScore &&  currentScore < randomScore) {
-            summedAmount = currentScore += parseInt(chosenAmount)
-            this.setState({ currentScore: summedAmount})
+        let { currentScore, chosenAmount, randomScore, bankAccount, counter } = this.state
+        let summedAmount = currentScore += parseInt(chosenAmount)
+        let takeOut;
+        if(parseInt(chosenAmount) < randomScore &&  summedAmount < randomScore) {
+            this.setState({ currentScore: summedAmount })
         } else {
-            this.setState({ currentScore: 0 })
+            takeOut = bankAccount -= currentScore
+            this.setState({ currentScore: 0, bankAccount: takeOut, counter: counter += 1 })
         }
 
+    }
+
+    onDeposit = (event) => {
+        event.preventDefault()
+        let { currentScore, bankAccount, counter } = this.state
+        let amount = (bankAccount += currentScore)
+        // let taxes = (amount * 0.3)
+        // let netPay = amount - taxes
+        this.setState({ bankAccount: amount, currentScore: 0, counter: counter += 1 })
     }
 
 
     render() {
         return (
             <div>
-                <div className="ui container">
-                {this.state.randomScore}
+
+                {console.log(this.state.randomScore)}
+            
+                <div style={{ position: "absolute", left: "450px", top: "200px" }} className="ui statistic">
+                    <div className="value">
+                           {this.state.currentScore}
+                    </div>
+                    <div className="label">
+                            Current Score
+                    </div>
+                </div>
+
+                <div style={{ position: "absolute", left: "800px", top: "250px" }} className="ui statistic">
+                    <div className="label">
+                            Bank Account
+                    </div>
+                    <div className="value">
+                            ${this.state.bankAccount}
+                    </div>
+                </div>
+
+            <div className="ui statistics">
+            <div className="red statistic" style={{position: "absolute", left: "1050px", top: "265px"}}>
+                <div className="value">
+                    {this.state.counter}
+                </div>
+                <div className="label">
+                    Counter
+                </div>
+            </div>
+            </div>
+
+                <div style={{ position: "absolute", left: "350px", top: "330px" }} className="ui container">
+
                 <form onSubmit={this.onSubmit}>
 
                 <div className="ui right labeled input">
@@ -48,13 +91,26 @@ class Game extends React.Component {
                     />
                 </div>
 
-                <button style={{ backgroundColor: "lightgreen", padding: "10px" }} type="submit">Submit</button>
+                <button 
+                style={{ backgroundColor: "lightgreen" }} type="submit" className="ui button">
+                    Submit
+                </button>
+
+                <br />
 
                 </form>
 
-        <p>{this.state.currentScore}</p>
+                <form onSubmit={this.onDeposit}>
+                    <div style={{ position: "absolute", top: "70px", left: "100px"}}>
+                        <button 
+                        style={{ backgroundColor: "lightblue" }} type="submit" className="ui button">
+                            Deposit
+                        </button>
+                    </div>
+                </form>
 
-        </div>
+
+                </div>
         </div>
         )
     }
