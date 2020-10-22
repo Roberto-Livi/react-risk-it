ef859c3bcd725cf9085126ff78b4e725696c08ad
Roberto-Livi <robertomlivi@outlook.com>
2020-10-22 13:15:47 -0400

    integrating redux, redux dev tools, and created a store, passing in a reducer and middleware

--- src/index.js
+++ src/index.js
@@ -1,8 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux'
+import { createStore, applyMiddleware, compose } from 'redux'
+import thunk from 'redux-thunk'
+import reducer from './reducers/manageGameData'
 import './index.css';
 import Login from './components/Login'
 
+const composeEnhancers = window._REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
 
-ReactDOM.render(<Login/>, document.getElementById('root'));
+const store = createStore(reducer, composeEnhancers(applyMiddleware(thunk)))
+
+ReactDOM.render(<Provider store={store}><Login/></Provider>, document.getElementById('root'));
 
