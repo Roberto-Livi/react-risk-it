7255bb4d8a62543c0f245934e1c688b3f9e017f2
Roberto-Livi <robertomlivi@outlook.com>
2020-10-22 13:59:55 -0400

    importing action types as constants and using them for the reducers switch statement

---
+++ src/actions/types.js
@@ -0,0 +1,8 @@
+export const LOG_IN = "LOG_IN";
+export const LOGGED_IN = "LOGGED_IN";
+export const CURRENT_SCORE = "CURRENT_SCORE";
+export const GHOST_NUMBER = "GHOST_NUMBER";
+export const TOTAL_SCORE = "TOTAL_SCORE";
+export const CHOSEN_AMOUNT = "CHOSEN_AMOUNT";
+export const COUNTER = "COUNTER";
+export const PREVIOUS_NUM = "PREVIOUS_NUM";
